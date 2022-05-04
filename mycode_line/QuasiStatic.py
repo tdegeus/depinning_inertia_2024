013e7848d180e3b7d72f3f798a5b979274785553
Tom de Geus <tom@geus.me>
2022-05-04 09:45:18 +0200

    Fixing selection of time-step

--- mycode_line/QuasiStatic.py
+++ mycode_line/QuasiStatic.py
@@ -254,9 +254,9 @@ def generate(file: h5py.File, N: int, seed: int, eta: float = None, dt: float = 
         eta = 1
         dt = 1
     elif dt is None:
-        np.array([1e-2, 1e-1, 1e0, 1e1, 1e2])
-        known_dt = np.array([1e-3, 5e-2, 5e-2, 5e-3, 1e-4])
-        dt = known_dt[np.argmin(np.abs(eta - known_dt))]
+        known_eta = np.array([1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2])
+        known_dt = np.array([5e-3, 1e-3, 5e-2, 5e-2, 5e-3, 5e-4])
+        dt = np.interp(eta, known_eta, known_dt)
     else:
         assert eta is not None
 
