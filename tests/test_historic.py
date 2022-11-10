3d715c760e41b8bf45207702ae3c46624e194cfd
Tom de Geus <tom@geus.me>
2022-11-10 10:24:15 +0100

    Adding historic test

---
+++ tests/test_historic.py
@@ -0,0 +1,70 @@
+import os
+import shutil
+import sys
+import unittest
+
+import GooseHDF5 as g5
+import h5py
+import numpy as np
+
+root = os.path.join(os.path.dirname(__file__), "..")
+if os.path.exists(os.path.join(root, "mycode_line", "_version.py")):
+    sys.path.insert(0, os.path.abspath(root))
+
+from mycode_line import Dynamics  # noqa: E402
+from mycode_line import EventMap  # noqa: E402
+from mycode_line import QuasiStatic  # noqa: E402
+
+dirname = os.path.join(os.path.dirname(__file__), "output")
+idname = "id=0000.h5"
+filename = os.path.join(dirname, idname)
+infoname = os.path.join(dirname, "EnsembleInfo.h5")
+historic = os.path.join(os.path.dirname(__file__), "test_historic_EnsembleInfo.h5")
+
+
+class MyTests(unittest.TestCase):
+    """ """
+
+    @classmethod
+    def setUpClass(self):
+
+        for file in [filename, infoname]:
+            if os.path.isfile(file):
+                os.remove(file)
+
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
+
+        QuasiStatic.cli_generate(["--dev", "--eta", 1e0, "-N", 50, "-n", 1, dirname])
+
+        with h5py.File(filename, "a") as file:
+            file["param"]["xyield"]["nchunk"][...] = 100
+            file["param"]["xyield"]["nbuffer"][...] = 20
+
+        QuasiStatic.cli_run(["--dev", "-n", 1000, filename])
+        QuasiStatic.cli_ensembleinfo(["--dev", "-o", infoname, filename])
+
+    @classmethod
+    def tearDownClass(self):
+        """
+        Remove the temporary directory.
+        """
+
+        shutil.rmtree(dirname)
+
+    def test_history(self):
+
+        ret = g5.compare(infoname, historic)
+
+        for key in ['/lookup/uuid', '/meta/QuasiStatic_EnsembleInfo']:
+            if key in ret["!="]:
+                ret["!="].remove(key)
+
+        self.assertEqual(ret["!="], [])
+        self.assertEqual(ret["->"], [])
+        self.assertEqual(ret["<-"], [])
+
+
+if __name__ == "__main__":
+
+    unittest.main(verbosity=2)
